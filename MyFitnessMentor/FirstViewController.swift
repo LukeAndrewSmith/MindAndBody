55b36d1099a829a862c2ba8f068dd4413edf63cf
Luke Smith <lukesmith@pc-136-211.customer.chamberlain.uob.lan>
2016-10-05 14:20:06 +0100

    Initial Commit

---
+++ MyFitnessMentor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyFitnessMentor
+//
+//  Created by Luke Smith on 05/10/16.
+//  Copyright © 2016 Luke Smith. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyFitnessMentor/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyFitnessMentor
+//
+//  Created by Luke Smith on 05/10/16.
+//  Copyright © 2016 Luke Smith. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
