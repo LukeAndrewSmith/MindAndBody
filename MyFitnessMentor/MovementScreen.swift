7f3e091de57430f8fe7e6020706825830fda6273
Luke Smith <lukesmith@lukes-macbook-air.home>
2016-11-26 16:27:19 +0100

    Periodic update

---
+++ MyFitnessMentor/MovementScreen.swift
@@ -0,0 +1,22 @@
+//
+//  MovementScreen.swift
+//  MyFitnessMentor
+//
+//  Created by Luke Smith on 26/11/16.
+//  Copyright © 2016 Luke Smith. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovementScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+}
