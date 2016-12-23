a5ac80c2f6e5795d5f02a875313289c3e5afbd20
Luke Smith <lukesmith@lukes-macbook-air.local>
2016-12-23 20:49:42 +0100

    23/12/16

--- MyFitnessMentor/WarmupChoice.swift
+++ MyFitnessMentor/WarmupChoice.swift
@@ -33,7 +33,8 @@ class WarmupChoice: UIViewController, UIScrollViewDelegate  {
     // Information View
     @IBOutlet weak var informationViewWarmupC: UIScrollView!
     
-    
+    // Information Title
+    @IBOutlet weak var informationTitleWarmupC: UILabel!
     
     
     override func viewDidLoad() {
@@ -89,8 +90,7 @@ class WarmupChoice: UIViewController, UIScrollViewDelegate  {
 //        downSwipe.direction = UISwipeGestureRecognizerDirection.down
 //        self.informationViewWarmupC.addGestureRecognizer(downSwipe)
         
-        self.informationViewWarmupC.frame = CGRect(x: 0, y: ((self.view.frame.size.height) - (self.navigationController?.navigationBar.frame.size.height)! - UIApplication.shared.statusBarFrame.height), width: self.view.frame.size.width, height: self.view.frame.size.height)
-        
+        self.informationViewWarmupC.frame = CGRect(x: 0, y: ((self.view.frame.size.height) - (self.navigationController?.navigationBar.frame.size.height)! - UIApplication.shared.statusBarFrame.height + 49), width: self.view.frame.size.width, height: (self.view.frame.size.height) - (self.navigationController?.navigationBar.frame.size.height)! - UIApplication.shared.statusBarFrame.height - 49)
         
         
         view.bringSubview(toFront: informationViewWarmupC)
@@ -99,45 +99,78 @@ class WarmupChoice: UIViewController, UIScrollViewDelegate  {
         
         
         // Information
-        let informationTitleWarmupC = UILabel(frame: CGRect(x: 0, y: 0, width: self.informationViewWarmupC.frame.size.width, height: 49))
+        self.informationTitleWarmupC.frame = CGRect(x: 0, y: ((self.view.frame.size.height) - (self.navigationController?.navigationBar.frame.size.height)! - UIApplication.shared.statusBarFrame.height), width: self.informationViewWarmupC.frame.size.width, height: 49)
         informationTitleWarmupC.text = (NSLocalizedString("information", comment: ""))
         informationTitleWarmupC.textAlignment = .center
         informationTitleWarmupC.font = UIFont(name: "SFUIDisplay-medium", size: 20)
         informationTitleWarmupC.textColor = .white
         informationTitleWarmupC.backgroundColor = UIColor(red:0.91, green:0.44, blue:0.25, alpha:1.0)
-        self.informationViewWarmupC.addSubview(informationTitleWarmupC)
+        self.view.addSubview(informationTitleWarmupC)
         
         
-        let informationTextWarmupC = UILabel(frame: CGRect(x: 20, y: 69, width: self.informationViewWarmupC.frame.size.width - 40, height: 0))
-        informationTextWarmupC.text = (
-            (NSLocalizedString("purpose", comment: ""))+"\n"+(NSLocalizedString("purposeText", comment: ""))+"\n"+"\n"+(NSLocalizedString("body", comment: ""))+"\n"+(NSLocalizedString("bodyText", comment: ""))+"\n"+"\n"+(NSLocalizedString("mind", comment: ""))+"\n"+(NSLocalizedString("mindText", comment: ""))
-            )
+        let informationTextWarmupC = UILabel(frame: CGRect(x: 20, y: 20, width: self.informationViewWarmupC.frame.size.width - 40, height: 0))
+        
+        
+        let informationLabelString = (
+            (NSLocalizedString("purpose", comment: ""))+"\n"+(NSLocalizedString("purposeText", comment: ""))+"\n"+"\n"+(NSLocalizedString("body", comment: ""))+"\n"+(NSLocalizedString("bodyText", comment: ""))+"\n"+"\n"+(NSLocalizedString("mind", comment: ""))+"\n"+(NSLocalizedString("mindText", comment: "")))
+        
+        //let titleAttribute = [NSFontAttributeName: UIFont(name: "SFUIDisplay-Medium", size: 19)]
+        
+        
+        let titleRange1 = (NSLocalizedString("purpose", comment: ""))
+        
+        let titleRange = (informationLabelString as NSString).range(of: titleRange1)
+        
+        let informationLabelText = NSMutableAttributedString(string: informationLabelString)
+    
+        //informationLabelText.addAttribute(NSParagraphStyleAttributeName, value: titleAttribute, range: titleRange)
+        
+        informationLabelText.addAttributes([NSFontAttributeName: UIFont(name: "SFUIDisplay-Medium", size: 19)!], range: titleRange)
+        
+        
+        
+        
+        informationTextWarmupC.attributedText = informationLabelText
+        
         informationTextWarmupC.textAlignment = .justified
         informationTextWarmupC.lineBreakMode = NSLineBreakMode.byWordWrapping
         informationTextWarmupC.numberOfLines = 0
         informationTextWarmupC.sizeToFit()
         informationTextWarmupC.font = UIFont(name: "SFUIDisplay-light", size: 19)
         self.informationViewWarmupC.addSubview(informationTextWarmupC)
-
+        
+        
+        self.informationViewWarmupC.contentSize = CGSize(width: self.view.frame.size.width, height: informationTextWarmupC.frame.size.height + informationTitleWarmupC.frame.size.height + 20)
+        
     }
     
     
     @IBAction func informationButtonActionWarmupC(_ sender: Any) {
         
-        if self.informationViewWarmupC.frame.maxY == (self.view.frame.maxY + ((self.view.frame.size.height))) {
+        if self.informationViewWarmupC.frame.maxY == (self.view.frame.maxY + ((self.view.frame.size.height) - (self.navigationController?.navigationBar.frame.size.height)! - UIApplication.shared.statusBarFrame.height)) {
             
             UIView.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
                 
                 self.informationViewWarmupC.transform = CGAffineTransform(translationX: 0, y: -((self.view.frame.size.height)))
                 
             }, completion: nil)
+            UILabel.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
             
-        } else if self.informationViewWarmupC.frame.maxY == self.view.frame.maxY {
+                self.informationTitleWarmupC.transform = CGAffineTransform(translationX: 0, y: -((self.view.frame.size.height)))
+            }, completion: nil)
+            self.informationViewWarmupC.contentOffset.y = 0
+            
+        } else if self.informationViewWarmupC.frame.maxY == (self.view.frame.maxY - (self.navigationController?.navigationBar.frame.size.height)! - UIApplication.shared.statusBarFrame.height) {
             UIView.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
                 
                 self.informationViewWarmupC.transform = CGAffineTransform(translationX: 0, y: 0)
                 
             }, completion: nil)
+            UILabel.animate(withDuration: 0.4, delay: 0.0, options: [], animations: {
+                
+                self.informationTitleWarmupC.transform = CGAffineTransform(translationX: 0, y: 0)
+            }, completion: nil)
+            self.informationViewWarmupC.contentOffset.y = 0
         }
 
     }
